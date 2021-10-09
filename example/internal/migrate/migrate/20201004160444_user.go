6d4c20b73fec70f0474d7965b5b3442c5652c4c1
nimoc <nimocc@126.com>
2021-10-09 22:45:21 +0800

    replace exec

--- example/internal/migrate/migrate/20201004160444_user.go
+++ example/internal/migrate/migrate/20201004160444_user.go
@@ -7,37 +7,24 @@ type Migrate struct {
 }
 
 func (Migrate) Migrate20201004160444CreateUserTable(mi sq.Migrate) {
-
-	mi.CreateTable(sq.CreateTableQB{
-		TableName: "user",
-		PrimaryKey: []string{"id"},
-		Fields: append([]sq.MigrateField{
-			mi.Field("id").Type("bigint",20).AutoIncrement(),
-			mi.Field("mobile").Char(11).DefaultString(""),
-			mi.Field("name").Char(20).DefaultString(""),
-			mi.Field("china_id_card_no").Char(18).Null(),
-		}, mi.CUDTimestamp()...),
-		Key: map[string][]string{
-			"mobile": {"mobile"},
-		},
-		UniqueKey: map[string][]string{
-			"china_id_card_no": []string{"china_id_card_no"},
-		},
-		Engine: mi.Engine().InnoDB,
-		Charset: mi.Charset().Utf8mb4,
-		Collate: mi.Utf8mb4_unicode_ci(),
-	})
-	mi.CreateTable(sq.CreateTableQB{
-		TableName: "user_address",
-		PrimaryKey: []string{"user_id"},
-		Fields: append([]sq.MigrateField{
-			mi.Field("user_id").Type("bigint", 20),
-			mi.Field("address").Varchar(255).DefaultString(""),
-		}, mi.CUDTimestamp()...),
-		Key: map[string][]string{},
-		UniqueKey: map[string][]string{},
-		Engine: mi.Engine().InnoDB,
-		Charset: mi.Charset().Utf8mb4,
-		Collate: mi.Utf8mb4_unicode_ci(),
-	})
+	mi.Exec(`
+	CREATE TABLE user (
+	  id char(36) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+	  name varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+	  age int(11) NOT NULL DEFAULT '0',
+	  created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	  updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+	  deleted_at timestamp NULL DEFAULT NULL,
+	  PRIMARY KEY (id),
+	  KEY name (name)
+	) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;`)
+	mi.Exec(`
+	CREATE TABLE user_address (
+	  user_id char(36) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+	  address varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+	  created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	  updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+	  deleted_at timestamp NULL DEFAULT NULL,
+	  PRIMARY KEY (user_id)
+	) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;`)
 }
