f8345aaaf31bd0ab6ad251f9d4cb4bcc8a91f91f
nimoc <nimocc@126.com>
2023-01-24 19:24:21 +0800

    Update migrate.go

--- migrate.go
+++ migrate.go
@@ -14,7 +14,7 @@ CREATE TABLE IF NOT EXISTS goclub_sql_migrations (
   id int(10) unsigned NOT NULL AUTO_INCREMENT,
   name varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (id)
+  PRIMARY KEY (id),
   UNIQUE KEY name (name)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci; 
 `
